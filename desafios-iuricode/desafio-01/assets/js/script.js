5fb299da2232c142accf41f6c779e4847293ea4c
MateusQuintanilha <100220698+MateusQuintanilha@users.noreply.github.com>
2022-05-12 16:51:29 -0300

    Adicionando postagens via js

--- desafios-iuricode/desafio-01/assets/js/script.js
+++ desafios-iuricode/desafio-01/assets/js/script.js
@@ -1,3 +1,49 @@
+const postagens = [
+    {
+        id: 01,
+        data: '02 de jul, 2021',
+        titulo: 'Agora é oficial: o Windows 11 está vindo',
+        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.'
+    },
+    {
+        id: 02,
+        data: '02 de jul, 2021',
+        titulo: 'Tim Berners-Lee vai leiloar código-fonte da web',
+        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.'
+    },
+    {
+        id: 03,
+        data: '02 de jul, 2021',
+        titulo: 'Tem Firefox novo no pedaço e você vai querer migrar',
+        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est.'
+    },
+    {
+        id: 04,
+        data: '02 de jul, 2021',
+        titulo: 'John McAfee, criador do antivírus McAfee, morre',
+        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.'
+    },
+];
+
+const containerPosts = document.querySelector('.posts');
+
+postagens.forEach(post => {
+containerPosts.innerHTML += `
+    <article class="post">
+        <section class="post-top-bar">
+            <span>${post.data}</span>
+            <div class="post-icon">
+                <img src="assets/img/icons/icon-heart.svg" alt="Ícone de curtida">
+            </div>
+        </section>
+        <section class="post-content">
+            <h2>${post.titulo}</h2>
+            <p>${post.texto}</p>
+        </section>
+    </article>
+`
+});
+
 document.addEventListener('click', (event) => {
     const element = event.target;
     
